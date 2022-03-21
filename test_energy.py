f7c45328bb1c3d8a72d2a5d01d26281f1681d669
Vinitha Marupeddi <vinithashruthi@gmail.com>
2022-03-21 16:30:17 -0700

    modified mpc to follow path

--- test_energy.py
+++ test_energy.py
@@ -1,13 +1,21 @@
+from importlib.resources import path
+from pickletools import pybool
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import torch.nn as nn
+import pybullet
+import math
+
 
 class FuncMinGTEnv():
-    def __init__(self, batch_size, input_size, batched_func):
+    def __init__(self, batch_size, input_size, batched_func, rob, path):
         self.a_size = input_size
         self.func = batched_func
         self.state = None
+        self.rob = rob
+        self.path = path
+        self.numInPath = 0
         self.reset_state(batch_size)
 
     def reset_state(self, batch_size):
@@ -23,6 +31,7 @@ class FuncMinGTEnv():
             total_r = total_r + r
             if(done):
                 break
+        self.numInPath += 1
         return total_r
 
     def step(self, action):
@@ -39,4 +48,11 @@ class FuncMinGTEnv():
         return None
 
     def calc_reward(self, state, action):
-        return -self.func(action)
+        x = pybullet.getJointInfo(self.rob, 0)[2]
+        y = pybullet.getJointInfo(self.rob, 0)[3]
+        z = pybullet.getJointInfo(self.rob, 0)[4]
+        x2 = path[self.numInPath][0]
+        y2 = path[self.numInPath][1]
+        z2 = path[self.numInPath][2]
+        distance = math.dist((x, y, z), (x2, y2, z2))
+        return distance
