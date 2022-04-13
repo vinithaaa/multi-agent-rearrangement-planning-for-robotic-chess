bdd83420afde0a6e1b17db1ee5e673eb2af2f235
Maheep <maheep.s.brar@gmail.com>
2022-04-13 18:26:30 -0400

    Changed reported final actions to be last one when it finishes, not the best rewards that might not complete

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def mlp_model(input, num_outputs, scope, reuse=False, num_units=64, rnn_cell=Non
 model = mlp_model
 
 
-arglist = {'max_episode_len': 20, 'num_episodes': 60000, 'num_adversaries': 0, 'good_policy': 'maddpg', 'lr': 1e-2, 'gamma': 0.95, 'batch_size': 1024, 'num_units': 64, 'collision_penalty': 2, 'action_duration' : 5}
+arglist = {'max_episode_len': 20, 'num_episodes': 60000, 'num_adversaries': 0, 'good_policy': 'maddpg', 'lr': 1e-2, 'gamma': 0.95, 'batch_size': 1024, 'num_units': 64, 'collision_penalty': 5, 'action_duration' : 5}
 
 
 with U.single_threaded_session():  
@@ -70,9 +70,8 @@ with U.single_threaded_session():
     t_start = time.time()
     t_curr = time.time()
     current_episode = 0
-    best_actions = []
     total_actions = []
-    best_reward = float('-inf')
+    best_actions = []
     f = open('printOutput.txt', 'a')
     converges = 0
 
@@ -100,10 +99,7 @@ with U.single_threaded_session():
             obs_n = env.reset()
             episode_step = 0
             print(episode_rewards[-1])
-            if (episode_rewards[-1] > best_reward):
-                best_actions = total_actions
-                best_reward = episode_rewards[-1]
-
+            best_actions = total_actions
             total_actions = []
             for a in agent_rewards:
                 a.append(0)
